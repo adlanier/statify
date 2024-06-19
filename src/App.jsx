58f05952532948b7346c293d89e1c7870bacf846
Adrian Lanier <111470566+adlanier@users.noreply.github.com>
2024-06-19 17:57:35 +0200

    loading and frontend

--- src/App.jsx
+++ src/App.jsx
@@ -97,204 +97,204 @@ function App() {
   };
 
 
-return (
-  <ChakraProvider>
-    <Flex direction="column" align="center" h="100vh" bg="black">
-      <Heading as="h1" color="green" textAlign="center" fontFamily="Proxima Nova" mt={30}>Statify</Heading>
-
-      <Flex justify="center" align="center" h="100vh" w="100%" className="App" p={5}>
-        {/* Box for current track */}
-        <Box
-          h="100%" 
-          w="100%" 
-          textAlign="center" 
-          p={5}
-          style={{
-            position: 'relative',
-            overflow: 'hidden',
-            borderRadius: '10px',
-            margin: '10px'
-          }}
-        >
-          {/* Background image with filter */}
-          <div
-            style={{
-              backgroundImage: `url(${currentTrack.cover})`,
-              backgroundSize: 'cover',
-              backgroundPosition: 'center',
-              filter: 'brightness(0.4)',
-              width: '100%',
-              height: '100%',
-              position: 'absolute',
-              top: 0,
-              left: 0,
-              zIndex: 1,
-              borderRadius: '10px'
-            }}
-          />
-          {/* Text content */}
-          <div
+  return (
+    <ChakraProvider>
+      <Flex direction="column" align="center" h="100vh" bg="black">
+        <Heading as="h1" color="green" textAlign="center" fontFamily="Proxima Nova" mt={6}>Statify</Heading>
+  
+        <Flex justify="center" align="center" h="100vh" w="100%" className="App" p={5}>
+          {/* Box for current track */}
+          <Box
+            h="100%" 
+            w="100%" 
+            textAlign="center" 
+            p={5}
             style={{
               position: 'relative',
-              top: 100,
-              zIndex: 2,
-              color: 'white',
-              textAlign: 'center',
-              padding: '20px',
-              fontSize: '2.2rem',
+              overflow: 'hidden',
+              borderRadius: '10px',
+              margin: '10px'
             }}
-            fontFamily="Proxima Nova"
           >
-            <b>{currentTrack.name}</b> by <b>{currentTrack.artists}</b>
-            <br />
-            has a popularity score of {currentTrack.popularity}
-          </div>
-        </Box>
-
-        {/* VS circle */}
-        {!gameOver && (
+            {/* Background image with filter */}
             <div
               style={{
+                backgroundImage: `url(${currentTrack.cover})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                filter: 'brightness(0.4)',
+                width: '100%',
+                height: '100%',
                 position: 'absolute',
-                background: 'green',
-                top: '50%',
-                left: '50%',
-                transform: 'translate(-50%, -50%)',
-                color: 'black',
-                borderRadius: '50%',
-                width: '80px',
-                height: '80px',
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                fontSize: '1.5rem',
-                fontWeight: 'bold',
-                zIndex: 20,
-fontFamily: 'Proxima Nova'
+                top: 0,
+                left: 0,
+                zIndex: 1,
+                borderRadius: '10px'
+              }}
+            />
+            {/* Text content */}
+            <div
+              style={{
+                position: 'relative',
+                top: 100,
+                zIndex: 2,
+                color: 'white',
+                textAlign: 'center',
+                padding: '20px',
+                fontSize: '2.2rem',
               }}
               fontFamily="Proxima Nova"
             >
-              VS
+              <b>{currentTrack.name}</b> by <b>{currentTrack.artists}</b>
+              <br />
+              has a popularity score of {currentTrack.popularity}
             </div>
-          )}
-
-        {/* Box for next track */}
-        <Box
-          h="100%" 
-          w="100%" 
-          textAlign="center"
-          p={5}
-          style={{
-            position: 'relative',
-            overflow: 'hidden',
-            borderRadius: '10px',
-            margin: '10px'
-          }}
-        >
-          {/* Background image with filter */}
-          <div
-            style={{
-              backgroundImage: `url(${nextTrack.cover})`,
-              backgroundSize: 'cover',
-              backgroundPosition: 'center',
-              filter: 'brightness(0.4)',
-              width: '100%',
-              height: '100%',
-              position: 'absolute',
-              top: 0,
-              left: 0,
-              zIndex: 1,
-              borderRadius: '10px'
-            }}
-          />
-          {/* Text content */}
-          <div
-            style={{
-              position: 'relative',
-              top: 100,
-              zIndex: 2,
-              color: 'white',
-              textAlign: 'center',
-              padding: '20px',
-              fontSize: '2.2rem',
-              fontFamily: 'Proxima Nova'
-            }}
-          >
-            {gameOver ? null : (
-              <div>
-                <div style={{ textAlign: 'center' }}><b>Does "{nextTrack.name}"</b> by <b>{nextTrack.artists}</b></div>
-                <div style={{ textAlign: 'center' }}>have a higher or lower popularity score?</div>
-                <div style={{ marginTop: '20px' }}>
-                <Stack spacing={4} direction="row" justify="center" mt={4}>
-                    <Button
-                      size="lg"
-                      colorScheme="whiteAlpha"
-                      onClick={() => handleGuess(true)}
-                      _hover={{ bg: "green.400", color: "white" }}
-                    >
-                      Higher &#128200;
-                    </Button>
-                    <Button
-                      size="lg"
-                      colorScheme="whiteAlpha"
-                      onClick={() => handleGuess(false)}
-                      _hover={{ bg: "green.400", color: "white" }}
-                    >
-                      Lower &#128201;
-                    </Button>
-                  </Stack>
-                </div>
+          </Box>
+  
+          {/* VS circle */}
+          {!gameOver && (
+              <div
+                style={{
+                  position: 'absolute',
+                  background: 'green',
+                  top: '50%',
+                  left: '50%',
+                  transform: 'translate(-50%, -50%)',
+                  color: 'black',
+                  borderRadius: '50%',
+                  width: '80px',
+                  height: '80px',
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  fontSize: '1.5rem',
+                  fontWeight: 'bold',
+                  zIndex: 20,
+  fontFamily: 'Proxima Nova'
+                }}
+                fontFamily="Proxima Nova"
+              >
+                VS
               </div>
             )}
-          </div>
-        </Box>
-      </Flex>
-
-     {/* Score Display */}
-      <div
-        style={{
-          position: 'fixed',
-          bottom: '20px',
-          right: '20px',
-          backgroundColor: 'green',
-          color: 'black',
-          borderRadius: '8px',
-          padding: '12px 24px',  // Increased padding for larger size
-          fontSize: '1.5rem',    // Increased font size for larger size
-          zIndex: 10,
-          fontFamily: 'Proxima Nova'
-        }}
-      >
-        Score: {score}
-      </div>
-
-      
-
-      {gameOver && (
-        <Box
-          position="fixed"
-          top="0"
-          left="0"
-          width="100vw"
-          height="100vh"
-          backgroundColor="rgba(0, 0, 0, 0.8)"
-          display="flex"
-          flexDirection="column"
-          justifyContent="center"
-          alignItems="center"
-          color="white"
-          zIndex="10"
-          fontFamily="Proxima Nova"
-        >
-          <Text fontSize="4xl" mb={4}>Game Over!</Text>
-          <Text fontSize="2xl" mb={8}>Your Score: {score}</Text>
-          <Button size="lg" colorScheme="whiteAlpha" onClick={resetGame}>Play Again</Button>
+  
+          {/* Box for next track */}
+          <Box
+            h="100%" 
+            w="100%" 
+            textAlign="center"
+            p={5}
+            style={{
+              position: 'relative',
+              overflow: 'hidden',
+              borderRadius: '10px',
+              margin: '10px'
+            }}
+          >
+            {/* Background image with filter */}
+            <div
+              style={{
+                backgroundImage: `url(${nextTrack.cover})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                filter: 'brightness(0.4)',
+                width: '100%',
+                height: '100%',
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                zIndex: 1,
+                borderRadius: '10px'
+              }}
+            />
+            {/* Text content */}
+            <div
+              style={{
+                position: 'relative',
+                top: 100,
+                zIndex: 2,
+                color: 'white',
+                textAlign: 'center',
+                padding: '20px',
+                fontSize: '2.2rem',
+                fontFamily: 'Proxima Nova'
+              }}
+            >
+              {gameOver ? null : (
+                <div>
+                  <div style={{ textAlign: 'center' }}><b>Does "{nextTrack.name}"</b> by <b>{nextTrack.artists}</b></div>
+                  <div style={{ textAlign: 'center' }}>have a higher or lower popularity score?</div>
+                  <div style={{ marginTop: '20px' }}>
+                  <Stack spacing={4} direction="row" justify="center" mt={4}>
+                      <Button
+                        size="lg"
+                        colorScheme="whiteAlpha"
+                        onClick={() => handleGuess(true)}
+                        _hover={{ bg: "green.400", color: "white" }}
+                      >
+                        Higher &#128200;
+                      </Button>
+                      <Button
+                        size="lg"
+                        colorScheme="whiteAlpha"
+                        onClick={() => handleGuess(false)}
+                        _hover={{ bg: "green.400", color: "white" }}
+                      >
+                        Lower &#128201;
+                      </Button>
+                    </Stack>
+                  </div>
+                </div>
+              )}
+            </div>
           </Box>
-      )}
-    </Flex>
-  </ChakraProvider>
-);
-}
+        </Flex>
+  
+       {/* Score Display */}
+        <div
+          style={{
+            position: 'fixed',
+            bottom: '20px',
+            right: '20px',
+            backgroundColor: 'green',
+            color: 'black',
+            borderRadius: '8px',
+            padding: '12px 24px',  // Increased padding for larger size
+            fontSize: '1.5rem',    // Increased font size for larger size
+            zIndex: 10,
+            fontFamily: 'Proxima Nova'
+          }}
+        >
+          Score: {score}
+        </div>
+  
+        
+  
+        {gameOver && (
+          <Box
+            position="fixed"
+            top="0"
+            left="0"
+            width="100vw"
+            height="100vh"
+            backgroundColor="rgba(0, 0, 0, 0.8)"
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+            color="white"
+            zIndex="10"
+            fontFamily="Proxima Nova"
+          >
+            <Text fontSize="4xl" mb={4}>Game Over!</Text>
+            <Text fontSize="2xl" mb={8}>Your Score: {score}</Text>
+            <Button size="lg" colorScheme="whiteAlpha" onClick={resetGame}>Play Again</Button>
+            </Box>
+        )}
+      </Flex>
+    </ChakraProvider>
+  );
+  }
 
 
 export default App;
